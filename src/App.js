435496fa52c51cfd23398b716d7fc5c734ffc9b7
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-09-08 16:51:29 +0200

    config projet moovice1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Weekly from "./components/Weekly.jsx";
+import WeeklyBattle from "./components/WeeklyBattle.jsx";
+import PopularBattle from "./components/PopularBattle.jsx";
+import Favorites from "./components/Favorites.jsx";
+import Error404 from "./components/Error404.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                Home
+              </a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                  <a class="nav-link active" aria-current="page" href="#">
+                    Weekly
+                  </a>
+                  <a class="nav-link" href="#">
+                    WeeklyBattle
+                  </a>
+                  <a class="nav-link" href="#">
+                    PopularBattle
+                  </a>
+                  <a class="nav-link" href="#">
+                    Favorites
+                  </a>
+                  <a
+                    class="nav-link disabled"
+                    href="#"
+                    tabindex="-1"
+                    aria-disabled="true"
+                  >
+                    Disabled
+                  </a>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Error404.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Error404 extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Error404</h1>
+      </>
+    );
+  }
+}
